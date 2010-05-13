d2c8904d68223b9498d51368d9df5c23a5424af0
cofd <cofd@localhost>
2010-05-13 10:32:26 +0800

    support async action

---
+++ src/Mvc/AsyncAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Kiss.Web.Mvc
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class AsyncAttribute : Attribute
+    {
+    }
+}
