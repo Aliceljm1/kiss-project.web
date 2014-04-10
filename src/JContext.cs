c3b1b0187ddf376baaee34ecae2cdf221488abed
zhli <zhli@localhost>
2014-04-10 19:48:38 +0800

    bug??

--- src/JContext.cs
+++ src/JContext.cs
@@ -427,6 +427,8 @@ namespace Kiss.Web
                     }
                     else
                     {
+                        _siteId = value;
+
                         SiteConfig = ServiceLocator.Instance.Resolve<IUserService>().GetSiteBySiteId(value);
 
                         if (SiteConfig != null)
@@ -453,8 +455,6 @@ namespace Kiss.Web
                                 Area.Theme = "default";
                         }
                     }
-
-                    _siteId = value;
                 }
             }
         }
