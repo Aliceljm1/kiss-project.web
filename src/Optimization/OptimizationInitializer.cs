41395a88a5cb9cac2ff7a00c7663caca3b5d557b
leixu <leixu@localhost>
2014-05-12 16:34:29 +0800

    ??? lazyinclude ??js ??? ??

---
+++ src/Optimization/OptimizationInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Kiss.Plugin;
+using Kiss.Utils;
+
+namespace Kiss.Web.Optimization
+{
+    [AutoInit]
+    class OptimizationInitializer : IPluginInitializer
+    {
+        public void Init(ServiceLocator sl, ref PluginSetting setting)
+        {
+            if (!setting.Enable) return;
+
+            OptimizationModule cm = new OptimizationModule();
+            cm.Start();
+            sl.AddComponentInstance(cm);
+
+            if (setting["css_sprite"].ToBoolean())
+                ImageOptimizations.AddCacheDependencies(ServerUtil.MapPath("~/"), true);
+        }
+    }
+}
---
+++ src/Optimization/minify/JavascriptMiniferStream.cs
@@ -0,0 +1,37 @@
+﻿using Kiss.Utils;
+using Kiss.Web.Utils;
+using System;
+using System.IO;
+using System.Web;
+
+namespace Kiss.Web.Optimization
+{
+    /// <summary>
+    /// minify javascript
+    /// </summary>
+    class JavascriptMinifierStream : HttpOutputFilter
+    {
+        /// <summary>
+        /// Creates a new Minify Filter for the provided stream
+        /// </summary>
+        public JavascriptMinifierStream(Stream stream)
+            : base(stream)
+        {
+            TransformString += (content) =>
+            {
+                try
+                {
+                    return new Kiss.Web.Utils.ajaxmin.Minifier().MinifyJavaScript(content);
+                }
+                catch (Exception ex)
+                {
+                    ILogger logger = LogManager.GetLogger<JavascriptMinifierStream>();
+                    logger.Error("error minify script:{0}.", HttpContext.Current.Request.RawUrl);
+                    logger.Error(ExceptionUtil.WriteException(ex));
+                }
+
+                return content;
+            };
+        }
+    }
+}
