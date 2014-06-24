453bb6d85c2bcc027cf319b8bb277abd1a7cf763
zhli <zhli@localhost>
2014-06-24 11:24:35 +0800

    _resc.aspx ??????????????null

---
+++ src/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("TXTEK")]
+[assembly: AssemblyCopyright("© TXTEK.CN - http://kiss.txtek.cn/")]
+[assembly: AssemblyProduct("KISS Project")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("4.4.0.*")]
+[assembly: AssemblyFileVersion("4.4.0.0")]
